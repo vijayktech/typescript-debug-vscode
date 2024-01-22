aa5571cf15c09bbc37a7fe55589f0186a6b6a837
Konkata Vijay Kumar (Trianz) <vijaykumar.konkata@trianz.com>
2024-01-22 19:05:05 +0530

    debugging ts file config

---
+++ out/hello.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.hello = void 0;
+/** Say hello */
+var hello = function (name) {
+    var greeting = "Hello ".concat(name, "!");
+    return greeting;
+};
+exports.hello = hello;
+//# sourceMappingURL=hello.js.map
